86c371e34311b6e15c2f89b6930e35d5d2cde96f
qwedxzc <163451723+qwedxzc@users.noreply.github.com>
2025-01-16 19:44:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const map = document.querySelector('#game');
+const leftCounter = document.querySelector('#leftCounter');
+const rightCounter = document.querySelector('#rightCounter');
+const leftCounterText = leftCounter.textContent;
+const rightCounterText = rightCounter.textContent;
 const canvas = map.getContext('2d');
 canvas.fillStyle = 'rgb(228, 164, 87)';
 
@@ -8,6 +12,8 @@ const maxPaddleY = map.height - grid - paddleHeight;
 
 let ballSpeed = 5;
 let paddleSpeed = 7;
+
+
 
 const leftPaddle = {
     x: grid * 2,
@@ -70,19 +76,123 @@ function moveBall() {
     ball.y += ball.dy;
 }
 
+function resetGame() {
+    if ((ball.x < 0 || ball.x > map.width) && !ball.isResetted) {
+        ball.isResetted = true;
+        setTimeout(() => {
+            ball.x = map.width / 2;
+            ball.y = map.height / 2;
+            ball.isResetted = false;
+        }, 1000);
+    }
+}
+
+function collideWallsWithPaddle(paddle) {
+    if (paddle.y < grid) {
+        paddle.y = grid;
+    }
+    else if (paddle.y > maxPaddleY) {
+        paddle.y = maxPaddleY;
+    }
+}
+
+function collideWallsWithPaddles() {
+    collideWallsWithPaddle(leftPaddle);
+    collideWallsWithPaddle(rightPaddle);
+}
+
+function collideWallsWithBall() {
+    if (ball.y < grid) {
+        ball.y = grid;
+        ball.dy = -ball.dy;
+    }
+    else if (ball.y > map.height - grid) {
+        ball.y = map.height - grid;
+        ball.dy = -ball.dy;
+    }
+}
+
+function isCollides(object1, object2) {
+    const width1 = object1.x + object1.width;
+    const width2 = object2.x + object2.width;
+    const height1 = object1.y + object1.height;
+    const height2 = object2.y + object2.height;
+    return object1.x < width2
+        && object2.x < width1
+        && object1.y < height2
+        && object2.y < height1;
+}
+
+function collidePaddlesWithBall() {
+    if (isCollides(ball, rightPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = rightPaddle.x - ball.width;
+        rightCounter.textContent = parseInt(rightCounter.textContent) + 1
+    }
+    else if (isCollides(ball, leftPaddle)) {
+        ball.dx = -ball.dx;
+        ball.x = leftPaddle.x + leftPaddle.width;
+        leftCounter.textContent = parseInt(leftCounter.textContent) + 1
+    }
+}
+
+function resetGame() {
+    if ((ball.x < 0 || ball.x > map.width) && !ball.isResetted) {
+
+        if (ball.x < 0) {
+            leftCounter.textContent = 0;
+        } else if (ball.x > map.width) {
+            rightCounter.textContent = 0;
+        }
+
+        ball.isResetted = true;
+        setTimeout(() => {
+            ball.x = map.width / 2;
+            ball.y = map.height / 2;
+            ball.isResetted = false;
+        }, 1000);
+    }
+}
+
+function aiControl() {
+    let direction = 0;
+
+    if (ball.y < rightPaddle.y) {
+        direction = -1;
+    }
+    else if (ball.y > rightPaddle.y + paddleHeight) {
+        direction = 1;
+    }
+
+    rightPaddle.y += paddleSpeed * direction;
+}
+
 function loop() {
     clearMap();
 
     renderLeftPaddle();
     renderRightPaddle();
+
+    aiControl()
+    
     movePaddles();
+
+    collideWallsWithPaddles();
 
     renderBall();
     moveBall();
 
+    collideWallsWithBall()
+    collidePaddlesWithBall()
+
+    resetGame();
     renderMap();
+
     requestAnimationFrame(loop);
+
+
 }
+
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'w' || event.key === 'ц') {
